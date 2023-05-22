8ee97f39707ae179208b8ecd37cffcf6c74d5362
Artur Gubaidullin <lapteuh@gmail.com>
2023-05-23 01:55:55 +0400

    fix(firestore)!: replace old adapter with new
    add conversation support
    fixes INVALID_ARGUMENT #167

--- packages/firestore/src/index.ts
+++ packages/firestore/src/index.ts
@@ -1,16 +1,4 @@
-import type { CollectionReference } from '@google-cloud/firestore';
+import { createAdapter } from './adapter';
+import { getBinaryEncoder } from './binary-encoder';
 
-export function adapter<T>(collection: CollectionReference) {
-  return {
-    read: async (key: string) => {
-      const snapshot = await collection.doc(key).get();
-      return snapshot.data() as T | undefined;
-    },
-    write: async (key: string, value: T) => {
-      await collection.doc(key).set(value);
-    },
-    delete: async (key: string) => {
-      await collection.doc(key).delete();
-    },
-  };
-}
+export const adapter = createAdapter(getBinaryEncoder());
